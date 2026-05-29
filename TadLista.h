225226d3314463494d58a1a0e202d0ead13e5ae1
joaovitortonello <joaovitortonello@alunos.utfpr.edu.br>
2026-05-28 22:26:23 -0300

    Update TadLista.h

--- TadLista.h
+++ TadLista.h
@@ -1,25 +1,19 @@
 #include "Bibliotecas.h"
 
-typedef struct LISTAD {
-    int chave;
-    struct LISTAD* noProx;
-    struct LISTAD* noAnt;
+typedef struct ListaS {
+
+    int id;
+    char Titulo[50];
+    char Artista[50];
+    int Ano;
+    char Album[50];
+    char TipoVersao[50];
+    int IdOriginal;
+
+    struct ListaS* NoProx;
+
 };
 
-#define true 1
-#define false 0
-
-
-
-void localizar(struct LISTAD** Localiza, int pos1, int codigo, struct LISTAD** RetornaELem);
-
-void buscarPosicao(int cod, struct LISTAD** pAuxIni, struct LISTAD** pAuxFim);
-
-void mostrar(struct LISTAD** Ini, int pos1);
-
-void remover(struct LISTAD** Ini, struct LISTAD** Fim);
-
-void inserir(struct LISTAD** Pri, struct LISTAD** Ult);
-
+void Adicionar();
 
 void Menu();
